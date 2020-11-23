0508c1be852c2911f418eef4879af6e0c2c51cbe
skiibot <ray-man100@hotmail.co.uk>
2020-11-23 08:32:46 +0000

    Add files via upload

---
+++ OldClientServer/TCPClientOld/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClientOld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //Socket client = null;
+            string hostIP = "127.0.0.1";
+            int portNo = 25000;
+
+            Socket client =  new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Generated socket instance");
+            IPEndPoint endPoint = new IPEndPoint(Dns.GetHostAddresses(hostIP)[0], portNo);
+            Console.WriteLine("Generated IPEndPoint instance that points to host IP socket");
+            client.Connect(endPoint);
+            while (true)
+            {            
+                Console.WriteLine("Connected to the server endpoint");
+                Console.WriteLine("What do you want to say?: ");
+
+                //start with the buffer 
+                string input = Console.ReadLine();
+                byte[] inputAsBytes = Encoding.ASCII.GetBytes(input);
+                Console.WriteLine("Converted what was written into bytes!");
+                client.Send(inputAsBytes, inputAsBytes.Length, SocketFlags.None);
+                Console.WriteLine($"Sent {inputAsBytes.Length} to the server!");
+                byte[] bytesFromServer = new byte[128];
+                int recievedInt = client.Receive(bytesFromServer);
+                string output = Encoding.ASCII.GetString(bytesFromServer, 0 ,recievedInt);
+                Console.WriteLine("Recieving data from server!");
+                Console.WriteLine($"Sever echoed back {output}");
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OldClientServer/TCPServerOld/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServerOld
+{
+    class Program
+    {
+        //public List<Socket> clientList = new List<Socket>();
+        static void Main(string[] args)
+        {
+            //Create new socket 
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Generated Socket that will accept client connections");
+            server.Bind(new IPEndPoint(IPAddress.Any, 25000));
+            Console.WriteLine("Bound the socket instance to any IP address that is using the 25000 port no");
+            server.Listen(5);
+            Console.WriteLine("start listening for connections! Clients can now connect to the server");
+            //start the server listening
+            //clientList = clientList.Add(server.Accept());
+            Socket client = server.Accept();
+            Console.WriteLine("Assign the client that we are listening in on to a new Socket instance!");
+            byte[] recieveBuffer = new byte[255];
+            int numRecievedBytes = 0;
+            int i = 0;
+            while (true)
+            {
+                numRecievedBytes = client.Receive(recieveBuffer);
+                Console.WriteLine(numRecievedBytes);
+                string recievedData = Encoding.ASCII.GetString(recieveBuffer,0, numRecievedBytes);
+                Console.WriteLine($"Recieved {recievedData}");
+                i++;
+                Console.WriteLine(i);
+                //Send data back to sende
+                byte[] sendBuffer = Encoding.ASCII.GetBytes(recievedData);
+                client.Send(sendBuffer);
+                Array.Clear(sendBuffer, 0, sendBuffer.Length);
+                Array.Clear(recieveBuffer, 0, recieveBuffer.Length);
+                Console.WriteLine("Sending data back to user!");
+
+
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
